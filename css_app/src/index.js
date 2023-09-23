9d5328a7161a83147649f425679f97176407ddd2
Ehmedi Al Omar <ehmedi.alomar.1@gmail.com>
2023-09-23 14:57:31 +0200

    clean css_app

--- css_app/src/index.js
+++ css_app/src/index.js
@@ -3,18 +3,13 @@ import * as d3 from "d3";
 
 const windowSize = 10;
 
-//const deviceApiKey = "d2a13a746cf437a91ddaba8eed70788e"; // Set your API key here
+// initialise the arrays to save the windows
 var x_data = new Array(windowSize).fill(0);
 var y_data = new Array(windowSize).fill(0);
 var z_data = new Array(windowSize).fill(0);
-// Reference to the toggle switch
+
+// get the html p element to write in it or to change style of it
 var texts = document.getElementsByTagName("p");
-
-for (let i = 0; i < texts.length; i++) {
-  texts[i].setAttribute("style", "font-size:" + 50 + "px");
-}
-
-// Add an event listener to the toggle switch
 
 window.addEventListener("devicemotion", (event) => {
   try {
@@ -39,6 +34,8 @@ window.addEventListener("devicemotion", (event) => {
   }
 });
 
+// prepare the window. calculate mean and variance and make prediction
+// and then change font size and in run change text
 function predict() {
   let x_mean = d3.mean(x_data);
   let x_var = d3.variance(x_data);
@@ -47,29 +44,32 @@ function predict() {
   let z_mean = d3.mean(z_data);
   let z_var = d3.variance(z_data);
   let pred = makePred([x_mean, x_var, y_mean, y_var, z_mean, z_var]);
-  var activity = "";
+  // standing
   if (pred == 1) {
     changeText(
       texts[0],
       " Lorem ipsum dolor sit amet consectetur adipisicing elit. "
     );
     changeFontSize(texts[0], 25);
+    // walking
   } else if (pred == 2) {
     changeText(
       texts[0],
       " Lorem ipsum dolor sit amet consectetur adipisicing elit. "
     );
     changeFontSize(texts[0], 50);
+    //running
   } else if (pred == 0) {
     changeText(texts[0], "you are running don't read now");
     changeFontSize(texts[0], 75);
   }
 }
-
+//change size of text
 function changeFontSize(p_element, fontSize) {
   p_element.setAttribute("style", "font-size:" + fontSize + "px");
 }
 
+//change text
 function changeText(p_element, newText) {
   p_element.innerText = newText;
 }
